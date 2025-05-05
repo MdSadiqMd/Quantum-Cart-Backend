2da5485fc5e481fabf9363ea14723ca66e291531
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-05 20:42:30 +0530

    🦿 added categeory model

---
+++ internal/models/Category.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Category struct {
+	ID           uint        `json:"id" gorm:"primary_key"`
+	Name         string      `json:"name" gorm:"index;not null"`
+	ParentId     uint        `json:"parent_id"`
+	ImageUrl     string      `json:"image_url"`
+	Products     interface{} `json:"products"`
+	DisplayOrder int         `json:"display_order"`
+	CreatedAt    time.Time   `json:"created_at" gorm:"default:current_timestamp"`
+	UpdatedAt    time.Time   `json:"updated_at" gorm:"default:current_timestamp"`
+	VoidedOn     time.Time   `json:"voided_on" gorm:"default:NULL"`
+}
