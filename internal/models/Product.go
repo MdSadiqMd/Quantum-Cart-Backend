65ee17d502119f93915069a2a90f2cd161e574ea
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-05 20:48:35 +0530

    🦾 added product model and one to one to categeory

---
+++ internal/models/Product.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Product struct {
+	ID          uint      `json:"id" gorm:"primary_key"`
+	Name        string    `json:"name" gorm:"index;not null"`
+	Description string    `json:"description"`
+	CategoryId  uint      `json:"category_id"`
+	ImageUrls   []string  `json:"image_urls"`
+	Price       float64   `json:"price"`
+	UserId      int       `json:"user_id"`
+	Stock       uint      `json:"stock"`
+	CreatedAt   time.Time `json:"created_at" gorm:"default:current_timestamp"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"default:current_timestamp"`
+	VoidedOn    time.Time `json:"voided_on" gorm:"default:NULL"`
+}
