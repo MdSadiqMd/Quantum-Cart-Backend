9c33bd1e86dc4555ca07877cdb53f1d7336d4561
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-11 18:52:14 +0530

    cart model refactor

--- internal/models/Cart.go
+++ internal/models/Cart.go
@@ -3,14 +3,15 @@ package models
 import "time"
 
 type Cart struct {
-	Id        uint      `json:"id" gorm:"primary_key"`
+	ID        uint      `gorm:"PrimaryKey" json:"id"`
 	UserId    uint      `json:"user_id"`
 	ProductId uint      `json:"product_id"`
 	Name      string    `json:"name"`
 	ImageUrl  string    `json:"image_url"`
+	SellerId  uint      `json:"seller_id"`
 	Price     float64   `json:"price"`
 	Quantity  uint      `json:"quantity"`
-	CreatedAt time.Time `json:"created_at" gorm:"default:current_timestamp"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"default:current_timestamp"`
-	VoidedOn  time.Time `json:"voided_on" gorm:"default:NULL"`
+	CreatedAt time.Time `gorm:"default:current_timestamp"`
+	UpdatedAt time.Time `gorm:"default:current_timestamp"`
+	VoidedOn  time.Time `gorm:"default:NULL"`
 }
